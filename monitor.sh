8724db256933c9809412d76756487cbc09f135f2
oarsaw <asvarsavabutbetter@gmail.com>
2025-12-01 20:42:47 +0300

    отладка

--- monitor.sh
+++ monitor.sh
@@ -6,12 +6,12 @@ while true; do
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
     if curl -s "http://localhost:${APP_PORT}" > /dev/null; then
-        echo "$TIMESTAMP — OK: приложение работает" >> "$LOG_FILE"
+        echo "$TIMESTAMP - OK: приложение работает" >> "$LOG_FILE"
     else
-        echo "$TIMESTAMP — ERROR: приложение недоступно, перезапускаем..." >> "$LOG_FILE"
+        echo "$TIMESTAMP - ERROR: приложение недоступно, перезапускаем" >> "$LOG_FILE"
         pkill -f "app.py" || true
         nohup bash -c "$APP_START_CMD" >> "$LOG_FILE" 2>&1 &
-        echo "$TIMESTAMP — Приложение перезапущено" >> "$LOG_FILE"
+        echo "$TIMESTAMP - Приложение перезапущено" >> "$LOG_FILE"
     fi
 
     sleep "$CHECK_INTERVAL"
